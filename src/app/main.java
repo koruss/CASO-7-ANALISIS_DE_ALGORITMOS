ed07f9237e9fc954f46367183eda0959c38a09be
Anner <anner.calvo@gmail.com>
2019-09-26 22:56:37 -0600

    Introdución de las llaves a una hash

--- src/app/main.java
+++ src/app/main.java
@@ -1,10 +1,14 @@
 package app;
+import java.util.HashSet;
+import java.util.Set;
+
 import app.AES;
 
 
 public class main {
 
 	public static void main(String[] args) {
+		Set<String> Keys = new HashSet<String>();
 		AES obj = new AES();
 		String p1=obj.keyp1;//la primera parte de las llaves
 		String p2=obj.keyp2;//la segunda parte de las llaves
@@ -15,19 +19,31 @@ public class main {
 		String message;
 		
 		for(int i=0; i<letras.length; i++) {
-			for(int j=0;j<(num.length);j++) {
-				newKey=p1+letras[i]+p2+num[j]+p3;
-				
-				message=obj.decrypt(newKey);
-				if(message!=null) {
-					System.out.println(newKey);
-					System.out.println(message);
-					
-				}
-				
-				
+			for(int j=0; j<num.length; j++) {
+				Keys.add(letras[i]+num[j]);
 			}
 		}
+		
+		int a=1;
+		for (String key : Keys) {
+			  System.out.println(a+" "+key);
+			  a++;
+			}
+		
+//		for(int i=0; i<letras.length; i++) {
+//			for(int j=0;j<(num.length);j++) {
+//				newKey=p1+letras[i]+p2+num[j]+p3;
+//				
+//				message=obj.decrypt(newKey);
+//				if(message!=null) {
+//					System.out.println(newKey);
+//					System.out.println(message);
+//					
+//				}
+//				
+//				
+//			}
+//		}
 		
 		
 
